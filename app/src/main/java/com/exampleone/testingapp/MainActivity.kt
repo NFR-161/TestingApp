2ea7d665469335c2f5e387517a8ee399ac525ccc
Николай <kolyas56atm@gmail.com>
2022-10-25 23:38:52 +0300

    add main pic, name, language and map icon

--- app/src/main/java/com/exampleone/testingapp/MainActivity.kt
+++ app/src/main/java/com/exampleone/testingapp/MainActivity.kt
@@ -3,11 +3,23 @@ package com.exampleone.testingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import com.exampleone.testingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+         ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        binding.toolbar.setTitleTextAppearance(this, R.style.InterRegularTextAppearance)
+
+
+
+
+
     }
 }
